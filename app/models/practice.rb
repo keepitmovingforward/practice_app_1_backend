ac84d2cafef976d6d8a1f82e033c6355b7ce4339
icydwei <robpaik@gmail.com>
2019-12-18 19:01:43 -0500

    learning to use benchmark

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,17 +1,31 @@
 require 'rspec'
+require 'benchmark'
 
-arr = Array.new(1000) {rand 1000 }
-puts arr.max
-
-arr.sort.last
+arr = Array.new(1000) {rand 1000}
 
 def new_max (array)
+  max_value = 0
 
-end
-
-describe 'New Max method' do
-  it 'returns the max value from an array efficiently' do
-    test_array = [2, 4, 10, 3, 1]
-    expect(new_max(test_array)).to eq(10)
+  array.each do |i|
+    if i > max_value
+      new_max = i
+    end
   end
+
+  max_value
 end
+
+Benchmark.bm(10) do |x|
+  x.report('Each: ') { new_max (arr)}
+  x.report('Sorted: ') {arr.sort.last }
+end
+
+# def new_max (array)
+# end
+#
+# describe 'New Max method' do
+#   it 'returns the max value from an array efficiently' do
+#     test_array = [2, 4, 10, 3, 1]
+#     expect(new_max(test_array)).to eq(10)
+#   end
+# end
