6b1a2f0b36c10ab11dcf4daeaa8779ad8c30bd79
icydwei <robpaik@gmail.com>
2019-12-18 19:08:00 -0500

    max is most efficient

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require 'benchmark'
 
-arr = Array.new(1000) {rand 1000}
+arr = Array.new(1000000) {rand 1000}
 
 def new_max (array)
   max_value = 0
@@ -17,15 +17,13 @@ end
 
 Benchmark.bm(10) do |x|
   x.report('Each: ') { new_max (arr)}
-  x.report('Sorted: ') {arr.sort.last }
+  # x.report('Sorted: ') {arr.sort.last }
+  x.report('Max: ') {arr.max}
 end
 
-# def new_max (array)
-# end
-#
-# describe 'New Max method' do
-#   it 'returns the max value from an array efficiently' do
-#     test_array = [2, 4, 10, 3, 1]
-#     expect(new_max(test_array)).to eq(10)
-#   end
-# end
+describe 'New Max method' do
+  it 'returns the max value from an array efficiently' do
+    test_array = [2, 4, 10, 3, 1]
+    expect(new_max(test_array)).to eq(10)
+  end
+end
