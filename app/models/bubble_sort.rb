074160d301c94d0d44cd6e81bc888088ce8b5e33
icydwei <robpaik@gmail.com>
2019-12-20 16:00:08 -0500

    bubble sorting

--- app/models/bubble_sort.rb
+++ app/models/bubble_sort.rb
@@ -1,1 +1,20 @@
 require 'rspec'
+
+class Array
+  def bubble_sort
+    n = self.length
+
+    loop do
+      
+    end
+  end
+
+end
+
+
+describe 'Adding bubble sort to the array class' do
+  it 'Properly sorts an array' do
+    arr = [4, 1, 6, 10, 44, 2, 3]
+    expect(arr.bubble_sort).to eq(arr.sort)
+  end
+end
