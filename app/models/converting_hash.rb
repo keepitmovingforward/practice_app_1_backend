d4edb34d9b88454075a1480ef6d1276a4ded40a1
icydwei <robpaik@gmail.com>
2019-12-23 14:21:38 -0500

    ruby * on hash is like a join with the character

--- app/models/converting_hash.rb
+++ app/models/converting_hash.rb
@@ -3,7 +3,7 @@ require 'rspec'
 class Hash
 
   def param_converter
-    self.map { |i| i * "=" } *"&"
+    self.map { |i| i * "=" } * "&"
   end
 
 end
@@ -11,7 +11,10 @@ end
 
 describe 'HTML param converter' do
   it 'Adds an HTML param converter to hash class' do
-    hash = {:topic => "baseball", :team=> "astros"}
-    expect (hash.param_converted).to eq('topic=baseball&team=astros')
+    hash = {:topic => "baseball", :team => "astros"}
+    expect(hash.param_converter).to eq('topic=baseball&team=astros')
   end
 end
+
+hash = {:topic => "baseball", :team=> "astros"}
+puts hash.param_converter
