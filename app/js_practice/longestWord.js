45eedf25b9dbacd138af73707d3ecb147444ee67
icydwei <robpaik@gmail.com>
2020-01-02 19:18:32 -0500

    js practice

---
+++ app/js_practice/longestWord.js
@@ -0,0 +1,13 @@
+function longestWord (str) {
+  let words = str.split(" ");
+  let longestWord = '';
+
+  for (word of words) {
+    if (word.)
+  }
+
+  return str;
+}
+
+console.log(longestWord("I woke up early today"));
+console.log(longestWord("I went straight to the beach"))
